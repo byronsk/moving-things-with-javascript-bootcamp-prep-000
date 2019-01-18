f8ed2e9930e00e3750a92dad6d32a27e24a45c7e
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-18 09:40:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const app = "I don't do much."
 
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
+
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
