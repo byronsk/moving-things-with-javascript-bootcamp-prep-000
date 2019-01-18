050870ed825db485e327f0b75c6bbc447a617249
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-18 09:39:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const app = "I don't do much."
 
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
